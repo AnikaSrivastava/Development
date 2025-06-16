5facb7e15b4ead6558cbedcbfaec1481f5c2301e
Anika Srivastava <anika.srivastava10@gmail.com>
2025-06-16 18:35:20 -0500

    Latest Leetcode solutions

---
+++ LeetCode75/src/main/java/org/example/leetcode75/IncreasingTriplet.java
@@ -0,0 +1,16 @@
+package org.example.leetcode75;
+
+public class IncreasingTriplet {
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        System.out.println(IncreasingTriplet.increasingTriplet(a));
+    }
+
+    //TODO
+    public static boolean increasingTriplet(int[] nums) {
+
+        return false;
+    }
+
+}
